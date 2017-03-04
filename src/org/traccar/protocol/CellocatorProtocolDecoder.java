traccar/traccar

Updated protocols to record additional information as reported in comments and confirmed in available documentation;

src/org/traccar/protocol/CellocatorProtocolDecoder.java
@@ -109,9 +109,9 @@ public class CellocatorProtocolDecoder extends BaseProtocolDecoder {
             }
             position.setDeviceId(deviceSession.getDeviceId());
 
-            buf.readUnsignedByte(); // hardware version
-            buf.readUnsignedByte(); // software version
-            buf.readUnsignedByte(); // protocol version
+            position.set("hardwareVersion", buf.readUnsignedByte()); // hardware version
+            position.set(Position.KEY_VERSION, buf.readUnsignedByte()); // software version
+            position.set("protocolVersion", buf.readUnsignedByte()); // protocol version
 
             position.set(Position.KEY_STATUS, buf.getUnsignedByte(buf.readerIndex()) & 0x0f);
 
@@ -121,21 +121,21 @@ public class CellocatorProtocolDecoder extends BaseProtocolDecoder {
             buf.readUnsignedByte(); // reason data
             position.set(Position.KEY_ALARM, decodeAlarm(buf.readUnsignedByte()));
 
-            buf.readUnsignedByte(); // mode
-            buf.readUnsignedInt(); // IO
+            position.set("mode", buf.readUnsignedByte()); // mode
+            position.set(Position.PREFIX_IO + 1, buf.readUnsignedInt()); // IO
 
             operator <<= 8;
             operator += buf.readUnsignedByte();
             position.set("operator", operator);
 
-            buf.readUnsignedInt(); // ADC
-            buf.readUnsignedMedium(); // Odometer
+            position.set(Position.PREFIX_ADC + 1, buf.readUnsignedInt()); // ADC
+            position.set(Position.KEY_ODOMETER, buf.readUnsignedMedium()); // Odometer
             buf.skipBytes(6); // multi-purpose data
 
-            buf.readUnsignedShort(); // gps fix
-            buf.readUnsignedByte(); // location status
-            buf.readUnsignedByte(); // mode 1
-            buf.readUnsignedByte(); // mode 2
+            position.set(Position.KEY_GPS, buf.readUnsignedShort()); // gps fix
+            position.set("locationStatus", buf.readUnsignedByte()); // location status
+            position.set("mode1", buf.readUnsignedByte()); // mode 1
+            position.set("mode2", buf.readUnsignedByte()); // mode 2
 
             int satellites = buf.readUnsignedByte();
             position.setValid(satellites >= 3);

src/org/traccar/protocol/SmokeyProtocolDecoder.java
@@ -84,11 +84,11 @@ public class SmokeyProtocolDecoder extends BaseProtocolDecoder {
 
         if (type == MSG_DATE_RECORD) {
 
-            buf.readUnsignedShort(); // firmware version
-
             Position position = new Position();
             position.setProtocol(getProtocolName());
             position.setDeviceId(deviceSession.getDeviceId());
+
+            position.set(Position.KEY_VERSION, buf.readUnsignedShort()); // firmware version
 
             int status = buf.readUnsignedShort();
             position.set(Position.KEY_STATUS, status);

src/org/traccar/protocol/TzoneProtocolDecoder.java
@@ -120,7 +120,7 @@ public class TzoneProtocolDecoder extends BaseProtocolDecoder {
             return null;
         }
         int hardware = buf.readUnsignedShort(); // model
-        buf.readUnsignedInt(); // firmware
+        long firmware = buf.readUnsignedInt(); // firmware
 
         String imei = ChannelBuffers.hexDump(buf.readBytes(8)).substring(1);
         DeviceSession deviceSession = getDeviceSession(channel, remoteAddress, imei);
@@ -131,6 +131,9 @@ public class TzoneProtocolDecoder extends BaseProtocolDecoder {
         Position position = new Position();
         position.setProtocol(getProtocolName());
         position.setDeviceId(deviceSession.getDeviceId());
+
+        position.set("hardwareVersion", hardware);
+        position.set(Position.KEY_VERSION, firmware);
 
         position.setDeviceTime(new DateBuilder()
                 .setDate(buf.readUnsignedByte(), buf.readUnsignedByte(), buf.readUnsignedByte())
@@ -199,10 +202,10 @@ public class TzoneProtocolDecoder extends BaseProtocolDecoder {
 
         if (blockLength >= 13) {
             position.set(Position.KEY_ALARM, decodeAlarm(buf.readUnsignedByte()));
-            buf.readUnsignedByte(); // terminal info
+            position.set("terminalInfo", buf.readUnsignedByte()); // terminal info
             position.set(Position.PREFIX_IO + 1, buf.readUnsignedShort());
             position.set(Position.KEY_RSSI, buf.readUnsignedByte());
-            buf.readUnsignedByte(); // GSM status
+            position.set("gsmStatus", buf.readUnsignedByte()); // GSM status
             position.set(Position.KEY_BATTERY, buf.readUnsignedShort());
             position.set(Position.KEY_POWER, buf.readUnsignedShort());
             position.set(Position.PREFIX_ADC + 1, buf.readUnsignedShort());

scortel-git/traccar-fork

src/org/traccar/protocol/CellocatorProtocolDecoder.java
@@ -109,9 +109,9 @@ public class CellocatorProtocolDecoder extends BaseProtocolDecoder {
             }
             position.setDeviceId(deviceSession.getDeviceId());
 
-            buf.readUnsignedByte(); // hardware version
-            buf.readUnsignedByte(); // software version
-            buf.readUnsignedByte(); // protocol version
+            position.set(Position.KEY_VERSION_HW, buf.readUnsignedByte()); // hardware version
+            position.set(Position.KEY_VERSION_FW, buf.readUnsignedByte()); // software version
+            position.set("protocolVersion", buf.readUnsignedByte()); // protocol version
 
             position.set(Position.KEY_STATUS, buf.getUnsignedByte(buf.readerIndex()) & 0x0f);
 
@@ -121,21 +121,21 @@ public class CellocatorProtocolDecoder extends BaseProtocolDecoder {
             buf.readUnsignedByte(); // reason data
             position.set(Position.KEY_ALARM, decodeAlarm(buf.readUnsignedByte()));
 
-            buf.readUnsignedByte(); // mode
-            buf.readUnsignedInt(); // io
+            position.set("mode", buf.readUnsignedByte()); // mode
+            position.set(Position.PREFIX_IO + 1, buf.readUnsignedInt()); // IO
 
             operator <<= 8;
             operator += buf.readUnsignedByte();
             position.set("operator", operator);
 
-            buf.readUnsignedInt(); // adc
-            buf.readUnsignedMedium(); // odometer
+            position.set(Position.PREFIX_ADC + 1, buf.readUnsignedInt()); // ADC
+            position.set(Position.KEY_ODOMETER, buf.readUnsignedMedium()); // Odometer
             buf.skipBytes(6); // multi-purpose data
 
-            buf.readUnsignedShort(); // gps fix
-            buf.readUnsignedByte(); // location status
-            buf.readUnsignedByte(); // mode 1
-            buf.readUnsignedByte(); // mode 2
+            position.set(Position.KEY_GPS, buf.readUnsignedShort()); // gps fix
+            position.set("locationStatus", buf.readUnsignedByte()); // location status
+            position.set("mode1", buf.readUnsignedByte()); // mode 1
+            position.set("mode2", buf.readUnsignedByte()); // mode 2
 
             position.set(Position.KEY_SATELLITES, buf.readUnsignedByte());
 

src/org/traccar/protocol/SmokeyProtocolDecoder.java
@@ -84,11 +84,11 @@ public class SmokeyProtocolDecoder extends BaseProtocolDecoder {
 
         if (type == MSG_DATE_RECORD) {
 
-            buf.readUnsignedShort(); // firmware version
-
             Position position = new Position();
             position.setProtocol(getProtocolName());
             position.setDeviceId(deviceSession.getDeviceId());
+
+            position.set(Position.KEY_VERSION_FW, buf.readUnsignedShort()); // firmware version
 
             int status = buf.readUnsignedShort();
             position.set(Position.KEY_STATUS, status);

src/org/traccar/protocol/TzoneProtocolDecoder.java
@@ -120,7 +120,7 @@ public class TzoneProtocolDecoder extends BaseProtocolDecoder {
             return null;
         }
         int hardware = buf.readUnsignedShort(); // model
-        buf.readUnsignedInt(); // firmware
+        long firmware = buf.readUnsignedInt(); // firmware
 
         String imei = ChannelBuffers.hexDump(buf.readBytes(8)).substring(1);
         DeviceSession deviceSession = getDeviceSession(channel, remoteAddress, imei);
@@ -131,6 +131,9 @@ public class TzoneProtocolDecoder extends BaseProtocolDecoder {
         Position position = new Position();
         position.setProtocol(getProtocolName());
         position.setDeviceId(deviceSession.getDeviceId());
+
+        position.set(Position.KEY_VERSION_HW, hardware);
+        position.set(Position.KEY_VERSION_FW, firmware);
 
         position.setDeviceTime(new DateBuilder()
                 .setDate(buf.readUnsignedByte(), buf.readUnsignedByte(), buf.readUnsignedByte())
@@ -199,10 +202,10 @@ public class TzoneProtocolDecoder extends BaseProtocolDecoder {
 
         if (blockLength >= 13) {
             position.set(Position.KEY_ALARM, decodeAlarm(buf.readUnsignedByte()));
-            buf.readUnsignedByte(); // terminal info
+            position.set("terminalInfo", buf.readUnsignedByte()); // terminal info
             position.set(Position.PREFIX_IO + 1, buf.readUnsignedShort());
             position.set(Position.KEY_RSSI, buf.readUnsignedByte());
-            buf.readUnsignedByte(); // GSM status
+            position.set("gsmStatus", buf.readUnsignedByte()); // GSM status
             position.set(Position.KEY_BATTERY, buf.readUnsignedShort());
             position.set(Position.KEY_POWER, buf.readUnsignedShort());
             position.set(Position.PREFIX_ADC + 1, buf.readUnsignedShort());